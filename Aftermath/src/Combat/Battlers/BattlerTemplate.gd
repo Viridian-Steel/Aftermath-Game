a90322c3489a655e1b95e0d605271ab1200cb7d7
Viridian Steel <56614399+Viridian-Steel@users.noreply.github.com>
2021-04-30 11:16:42 -0500

    Add files via upload

---
+++ Aftermath/src/Combat/Battlers/BattlerTemplate.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name BattlerTemplate
+
+export var anim: PackedScene
+export var stats: Resource
+export var skills: Array
+
+#use Array of Dictionaries(Structs) for Drops
+export var drops: Array
